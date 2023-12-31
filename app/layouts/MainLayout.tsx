318cf098f2468b2c67e12f138f5b80e1733ec9e4
felipeversiane <felipeversiane09@gmail.com>
2023-12-31 19:10:12 -0300

    :pencil: initial commit

---
+++ app/layouts/MainLayout.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from 'react'
+import Head from 'next/head'
+
+
+type Props = {
+  children?: ReactNode
+  title?: string
+}
+
+export default function MainLayout({ children, title = 'Title' }: Props){
+  return (
+  <>
+    <Head>
+      <title>
+        {title}
+      </title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    {children}
+  </>
+);
+};
---
+++ app/pages/index.tsx
@@ -0,0 +1,11 @@
+import MainLayout from '../layouts/MainLayout';
+
+
+const Index = () => {
+  return (
+  <MainLayout title='Page title'>
+    Your Content here...
+  </MainLayout>
+  );
+};
+export default Index;
